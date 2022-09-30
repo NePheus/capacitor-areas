75239f9adaaf89f4ee235ed8c01829e56827ee11
Jan Gessinger <jan@vi-a.de>
2022-09-30 19:36:18 +0200

    Added SDK version check to windowInsets + refactored method for navigation height

--- android/src/main/java/nepheus/capacitor/areas/AreasPlugin.java
+++ android/src/main/java/nepheus/capacitor/areas/AreasPlugin.java
@@ -1,8 +1,14 @@
 package nepheus.capacitor.areas;
 
+import android.content.Context;
 import android.content.res.Resources;
+import android.graphics.Point;
+import android.os.Build;
+import android.view.Display;
 import android.view.DisplayCutout;
 import android.view.WindowInsets;
+import android.view.WindowManager;
+
 import com.getcapacitor.JSObject;
 import com.getcapacitor.Plugin;
 import com.getcapacitor.PluginCall;
@@ -13,60 +19,89 @@ import com.getcapacitor.annotation.CapacitorPlugin;
 public class AreasPlugin extends Plugin {
     @PluginMethod
     public void getSafeArea(PluginCall call) {
-        WindowInsets windowInsets = this.getBridge().getActivity().getWindow().getDecorView().getRootWindowInsets();
-        if (windowInsets == null) {
-            call.reject("WindowInsets not available");
-            return;
-        }
+        int top = 0;
+        int left = 0;
+        int bottom = 0;
+        int right = 0;
 
-        DisplayCutout displayCutout = windowInsets.getDisplayCutout();
-        if (displayCutout == null) {
-            call.reject("DisplayCutout not available");
-            return;
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.P) {
+            WindowInsets windowInsets =
+                    this.getBridge().getActivity().getWindow().getDecorView().getRootWindowInsets();
+            if (windowInsets == null) {
+                call.reject("WindowInsets not available");
+                return;
+            }
+
+            DisplayCutout displayCutout = windowInsets.getDisplayCutout();
+            if (displayCutout == null) {
+                call.reject("DisplayCutout not available");
+                return;
+            }
+
+            top = this._convertToDeviceHeight(displayCutout.getSafeInsetTop());
+            left = this._convertToDeviceHeight(displayCutout.getSafeInsetLeft());
+            bottom = this._convertToDeviceHeight(displayCutout.getSafeInsetBottom());
+            right = this._convertToDeviceHeight(displayCutout.getSafeInsetRight());
         }
 
         JSObject ret = new JSObject();
-        ret.put("top", this.convertDimension(displayCutout.getSafeInsetTop()));
-        ret.put("left", this.convertDimension(displayCutout.getSafeInsetLeft()));
-        ret.put("bottom", this.convertDimension(displayCutout.getSafeInsetBottom()));
-        ret.put("right", this.convertDimension(displayCutout.getSafeInsetRight()));
+        ret.put("top", top);
+        ret.put("left", left);
+        ret.put("bottom", bottom);
+        ret.put("right", right);
         call.resolve(ret);
     }
 
     @PluginMethod
     public void getStatusBarHeight(PluginCall call) {
-        Resources resources = getActivity().getResources();
-
-        float dimension = this.convertDimension(
-                resources.getDimension(
-                        resources.getIdentifier("status_bar_height", "dimen", "android")
-                ));
-
         JSObject ret = new JSObject();
-        ret.put("value", dimension);
+        ret.put("value", this._getStatusBarHeight());
         call.resolve(ret);
     }
 
     @PluginMethod
     public void getNavigationBarHeight(PluginCall call) {
-        Resources resources = getActivity().getResources();
+        WindowManager windowManager =
+                (WindowManager) getContext().getSystemService(Context.WINDOW_SERVICE);
+        int height = 0;
+        if (Build.VERSION.SDK_INT >= 30) {
+            height = this._convertToDeviceHeight(windowManager
+                    .getCurrentWindowMetrics()
+                    .getWindowInsets()
+                    .getInsets(WindowInsets.Type.navigationBars())
+                    .bottom);
+        } else {
+            Display display = windowManager.getDefaultDisplay();
+            Point appUsableSize = new Point();
+            display.getSize(appUsableSize);
+            Point realScreenSize = new Point();
+            display.getRealSize(realScreenSize);
 
-        int id = resources.getIdentifier("config_showNavigationBar", "bool", "android");
+            if (appUsableSize.y < realScreenSize.y) {
+                // navigation bar at the bottom
+                height = this._convertToDeviceHeight(realScreenSize.y - appUsableSize.y);
+            } else if (appUsableSize.x < realScreenSize.x) {
+                // navigation bar on the side
+                height = this._convertToDeviceHeight(appUsableSize.y);
+            }
 
-        float dimension = 0;
-        if (id > 0 && resources.getBoolean(id)) {
-            dimension = this.convertDimension(
-                    resources.getDimension(
-                            resources.getIdentifier("navigation_bar_height", "dimen", "android")
-                    ));
+            height -= _getStatusBarHeight();
         }
 
         JSObject ret = new JSObject();
-        ret.put("value", dimension);
+        ret.put("value", height);
         call.resolve(ret);
     }
 
-    private int convertDimension(float dimension) {
-        return (int) Math.ceil(dimension / this.getContext().getResources().getDisplayMetrics().density);
+    private int _getStatusBarHeight() {
+        Resources resources = getActivity().getResources();
+        return this._convertToDeviceHeight(
+                resources.getDimension(
+                        resources.getIdentifier("status_bar_height", "dimen", "android")
+                ));
+    }
+
+    private int _convertToDeviceHeight(float height) {
+        return (int) Math.ceil(height / this.getContext().getResources().getDisplayMetrics().scaledDensity);
     }
 }
